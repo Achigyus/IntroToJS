f678518da8f3ee39e279242e16e4ef6a32d39de7
Achigyus <gyuseachi@yahoo.com>
2022-06-24 10:02:19 +0100

    done

---
+++ index.js
@@ -0,0 +1,6 @@
+let myName = "Achi Gyuse Achi"
+let height = 5.9
+let country = "Nigeria"
+
+alert(`Hello There 👋. My Name is ${myName}, my height is ${height}, and I live in ${country}`)
+console.log(`Hello There 👋. My Name is ${myName}, my height is ${height}, and I live in ${country}`)
